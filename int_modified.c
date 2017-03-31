1db5d2267d45019d640d8d4f80db86a3b97277c2
FloraCsWang <mw1997@nyu.edu>
2017-03-31 01:21:51 -0400

    fix int_modified bug

--- int_modified.c
+++ int_modified.c
@@ -15,30 +15,40 @@ int main(int argc, char *argv[])
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Status status;
-    char* message_in;
-    char* message_out;
+    int Z = 2000000;
+    int n;
+    char *message_out = calloc(Z, sizeof(char));
+    char *message_in = calloc(Z, sizeof(char));
+  
+    for(n = 0; n < Z; ++n)
+        message_out[n] = 'h';
+    
     for (int i = 0; i < N; i++){
    
         if (rank == 0)
         {
-            message_out = "HELLO";
+            //message_out = 0 ;
             destination = 1;
             origin = 5;
-            MPI_Send(message_out, 2000000, MPI_CHAR, destination, tag, MPI_COMM_WORLD);
-            MPI_Recv(message_in,  2000000, MPI_CHAR, origin,      tag, MPI_COMM_WORLD, &status);
+            MPI_Send(message_out, Z, MPI_CHAR, destination, tag, MPI_COMM_WORLD);
+            MPI_Recv(message_in,  Z, MPI_CHAR, origin,      tag, MPI_COMM_WORLD, &status);
+            printf("the final message is %s \n", message_in);
         } else  {
-            message_in = message_out;
+            
             destination = (rank + 1) % 6;
             origin = rank -1;
-            MPI_Recv(message_in,  2000000, MPI_CHAR, origin,      tag, MPI_COMM_WORLD, &status);
-            MPI_Send(message_out, 2000000, MPI_CHAR, destination, tag, MPI_COMM_WORLD);
+            MPI_Recv(message_in,  Z, MPI_CHAR, origin,      tag, MPI_COMM_WORLD, &status);
+            message_out = message_in;
+            MPI_Send(message_out, Z, MPI_CHAR, destination, tag, MPI_COMM_WORLD);
         }
     }
     MPI_Finalize();
     
-    get_timestamp(&time2);
-    double elapsed = timestamp_diff_in_seconds(time1,time2);
-    printf("Time elapsed is %f seconds.\n", elapsed);
+    if (rank == 0){
+        get_timestamp(&time2);
+        double elapsed = timestamp_diff_in_seconds(time1,time2);
+        printf("Time elapsed is %f seconds.\n", elapsed);
+    }
     
     
     
